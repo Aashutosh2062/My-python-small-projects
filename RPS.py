2b5dca6adfb47422f3884901577fb990b815c0c6
Aashutosh Khatiwada <khatiwadaaashutosh30@gmail.com>
2025-12-19 18:25:04 +0545

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -4,7 +4,7 @@ num1 = random.randint(0,2)
 #0=rock
 #1=paper
 #2=scissor
-print(num1)
+
 
 def greet(name):
     if((name=="rock" and num1==0) or (name=="paper" and num1==1) or (name=="scissor" and num1==2)):
@@ -13,9 +13,7 @@ def greet(name):
         print("you won")
     else:
         print("you lost")
-    
-
-    
-    
+        
 
 greet(name)
+
